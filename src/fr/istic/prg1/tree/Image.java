92e78dcb1ef312833f1ba5f527d563bcd4f96f38
Melen Laclais <145654016+MelenL@users.noreply.github.com>
2024-11-27 10:32:37 +0100

    VideoInverse et AffectAux amélioré

--- src/fr/istic/prg1/tree/Image.java
+++ src/fr/istic/prg1/tree/Image.java
@@ -11,263 +11,256 @@ import fr.istic.prg1.tree_util.NodeType;
  * @author Mickaël Foursov <foursov@univ-rennes1.fr>
  * @version 5.0
  * @since 2023-09-23
- * 
- *        Classe décrivant les images en noir et blanc de 256 sur 256 pixels
- *        sous forme d'arbres binaires.
- * 
+ * <p>
+ * Classe décrivant les images en noir et blanc de 256 sur 256 pixels
+ * sous forme d'arbres binaires.
  */
 
 public class Image extends AbstractImage {
-	private static final Scanner standardInput = new Scanner(System.in);
+    private static final Scanner standardInput = new Scanner(System.in);
 
-	public Image() {
-		super();
-	}
+    public Image() {
+        super();
+    }
 
-	public static void closeAll() {
-		standardInput.close();
-	}
+    public static void closeAll() {
+        standardInput.close();
+    }
 
-	/**
-	 * this devient identique à image2.
-	 *
-	 * @param image2 image à copier
-	 *
-	 * @pre !image2.isEmpty()
-	 */
-	@Override
-	public void affect(AbstractImage image2) {
-		Iterator<Node> it1 = this.iterator();// Il devrait manquer une ligne sinon erreur
-		Iterator<Node> it2 = image2.iterator();
-		affectAux(it1,it2);
-	}
+    /**
+     * this devient identique à image2.
+     *
+     * @param image2 image à copier
+     * @pre !image2.isEmpty()
+     */
+    @Override
+    public void affect(AbstractImage image2) {
+        Iterator<Node> it1 = this.iterator();// Il devrait manquer une ligne sinon erreur
+        Iterator<Node> it2 = image2.iterator();
+        it1.clear();
+        affectAux(it1, it2);
+    }
 
-	public void affectAux(Iterator<Node>it1,Iterator<Node>it2 ) {
-		if (!it2.isEmpty()){
-			// Traitement des cas
-			Node node = it1.getValue();
-			Node nodeImage = it2.getValue();
-			if (it1.isEmpty() ){                // Cas 2 : si it1 est vide on affecte it2
-				it1.addValue(nodeImage);
-			}
-			else {
-				it2.setValue(nodeImage);
-			}
-			// Parcours
-			it1.goLeft();it2.goLeft();
-			affectAux(it1,it2);
-			it1.goUp();	it2.goUp();
-			it1.goRight();it2.goRight();
-			affectAux(it1,it2);
-			it1.goUp();it2.goUp();
-		}
+    public void affectAux(Iterator<Node> it1, Iterator<Node> it2) {
+        Node node = it2.getValue();
+        it1.addValue(node);
 
-		else{
-			while(!it1.isEmpty())it1.remove();   //Cas 1 : Tant qu'il reste des valeurs dans l'arbre 1 on supprime
-		}
-	}
+        if (it2.getValue().state == 2) {
+            it1.addValue(Node.valueOf(2));
 
-	/**
-	 * this devient rotation de image2 à 180 degrés.
-	 *
-	 * @param image2 image pour rotation
-	 * @pre !image2.isEmpty()
-	 */
-	@Override
-	public void rotate180(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-	}
+            it1.goLeft();
+            it2.goLeft();
+            affectAux(it1, it2);
+            it1.goUp();
+            it2.goUp();
+            it1.goRight();
+            it2.goRight();
+            affectAux(it1, it2);
+            it1.goUp();
+            it2.goUp();
+        }
+    }
 
-	/**
-	 * this devient inverse vidéo de this, pixel par pixel.
-	 *
-	 * @pre !image.isEmpty()
-	 */
-	@Override
-	public void videoInverse() {
-		Iterator<Node> it = this.iterator();
-		videoInverseAux(it);
-	}
-	private void videoInverseAux(Iterator <Node> it) {
-		if(! it.isEmpty()){
-			if(it.nodeType() == NodeType.LEAF){
-				Node currentNode = it.getValue();
-				if(currentNode.state==1)it.setValue(Node.valueOf(0));
-				else  it.setValue(Node.valueOf(1));
-			}
-			it.goLeft();
-			videoInverseAux(it);
-			it.goUp();
-			it.goRight();
-			videoInverseAux(it);
-			it.goUp();
-		}
-	}
+    /**
+     * this devient rotation de image2 à 180 degrés.
+     *
+     * @param image2 image pour rotation
+     * @pre !image2.isEmpty()
+     */
+    @Override
+    public void rotate180(AbstractImage image2) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+    }
 
-	/**
-	 * this devient image miroir verticale de image2.
-	 *
-	 * @param image2 image à agrandir
-	 * @pre !image2.isEmpty()
-	 */
-	@Override
-	public void mirrorV(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-	}
+    /**
+     * this devient inverse vidéo de this, pixel par pixel.
+     *
+     * @pre !image.isEmpty()
+     */
+    @Override
+    public void videoInverse() {
+        Iterator<Node> it = this.iterator();
+        videoInverseAux(it);
+    }
 
-	/**
-	 * this devient image miroir horizontale de image2.
-	 *
-	 * @param image2 image à agrandir
-	 * @pre !image2.isEmpty()
-	 */
-	@Override
-	public void mirrorH(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-	}
+    private void videoInverseAux(Iterator<Node> it) {
+        Node currentNode = it.getValue();
+            if (currentNode.state == 1) {
+                it.setValue(Node.valueOf(0));
+            }
+            else if (currentNode.state == 0) {
+                it.setValue(Node.valueOf(1));
+            } else {
+            it.goLeft();
+            videoInverseAux(it);
+            it.goUp();
+            it.goRight();
+            videoInverseAux(it);
+            it.goUp();
+        }
+    }
 
-	/**
-	 * this devient quart supérieur gauche de image2.
-	 *
-	 * @param image2 image à agrandir
-	 * 
-	 * @pre !image2.isEmpty()
-	 */
-	@Override
-	public void zoomIn(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-	}
+    /**
+     * this devient image miroir verticale de image2.
+     *
+     * @param image2 image à agrandir
+     * @pre !image2.isEmpty()
+     */
+    @Override
+    public void mirrorV(AbstractImage image2) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+    }
 
-	/**
-	 * Le quart supérieur gauche de this devient image2, le reste de this devient
-	 * éteint.
-	 * 
-	 * @param image2 image à réduire
-	 * @pre !image2.isEmpty()
-	 */
-	@Override
-	public void zoomOut(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-	}
+    /**
+     * this devient image miroir horizontale de image2.
+     *
+     * @param image2 image à agrandir
+     * @pre !image2.isEmpty()
+     */
+    @Override
+    public void mirrorH(AbstractImage image2) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+    }
 
-	/**
-	 * this devient l'intersection de image1 et image2 au sens des pixels allumés.
-	 * 
-	 * @pre !image1.isEmpty() && !image2.isEmpty()
-	 * 
-	 * @param image1 premiere image
-	 * @param image2 seconde image
-	 */
-	@Override
-	public void intersection(AbstractImage image1, AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-	}
+    /**
+     * this devient quart supérieur gauche de image2.
+     *
+     * @param image2 image à agrandir
+     * @pre !image2.isEmpty()
+     */
+    @Override
+    public void zoomIn(AbstractImage image2) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+    }
 
-	/**
-	 * this devient l'union de image1 et image2 au sens des pixels allumés.
-	 * 
-	 * @pre !image1.isEmpty() && !image2.isEmpty()
-	 * 
-	 * @param image1 premiere image
-	 * @param image2 seconde image
-	 */
-	@Override
-	public void union(AbstractImage image1, AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-	}
+    /**
+     * Le quart supérieur gauche de this devient image2, le reste de this devient
+     * éteint.
+     *
+     * @param image2 image à réduire
+     * @pre !image2.isEmpty()
+     */
+    @Override
+    public void zoomOut(AbstractImage image2) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+    }
 
-	/**
-	 * Attention : cette fonction ne doit pas utiliser la commande isPixelOn
-	 * 
-	 * @return true si tous les points de la forme (x, x) (avec 0 <= x <= 255)
-	 *         sont allumés dans this, false sinon
-	 */
-	@Override
-	public boolean testDiagonal() {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-		return false;
-	}
+    /**
+     * this devient l'intersection de image1 et image2 au sens des pixels allumés.
+     *
+     * @param image1 premiere image
+     * @param image2 seconde image
+     * @pre !image1.isEmpty() && !image2.isEmpty()
+     */
+    @Override
+    public void intersection(AbstractImage image1, AbstractImage image2) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+    }
 
-	/**
-	 * @param x abscisse du point
-	 * @param y ordonnée du point
-	 * @pre !this.isEmpty()
-	 * @return true, si le point (x, y) est allumé dans this, false sinon
-	 */
-	@Override
-	public boolean isPixelOn(int x, int y) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-		return false;
-	}
+    /**
+     * this devient l'union de image1 et image2 au sens des pixels allumés.
+     *
+     * @param image1 premiere image
+     * @param image2 seconde image
+     * @pre !image1.isEmpty() && !image2.isEmpty()
+     */
+    @Override
+    public void union(AbstractImage image1, AbstractImage image2) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+    }
 
-	/**
-	 * @param x1 abscisse du premier point
-	 * @param y1 ordonnée du premier point
-	 * @param x2 abscisse du deuxième point
-	 * @param y2 ordonnée du deuxième point
-	 * @pre !this.isEmpty()
-	 * @return true si les deux points (x1, y1) et (x2, y2) sont représentés par la
-	 *         même feuille de this, false sinon
-	 */
-	@Override
-	public boolean sameLeaf(int x1, int y1, int x2, int y2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-		return false;
-	}
+    /**
+     * Attention : cette fonction ne doit pas utiliser la commande isPixelOn
+     *
+     * @return true si tous les points de la forme (x, x) (avec 0 <= x <= 255)
+     * sont allumés dans this, false sinon
+     */
+    @Override
+    public boolean testDiagonal() {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+        return false;
+    }
 
-	/**
-	 * @param image2 autre image
-	 * @pre !this.isEmpty() && !image2.isEmpty()
-	 * @return true si this est incluse dans image2 au sens des pixels allumés false
-	 *         sinon
-	 */
-	@Override
-	public boolean isIncludedIn(AbstractImage image2) {
-		System.out.println();
-		System.out.println("-------------------------------------------------");
-		System.out.println("Fonction a ecrire");
-		System.out.println("-------------------------------------------------");
-		System.out.println();
-		return false;
-	}
+    /**
+     * @param x abscisse du point
+     * @param y ordonnée du point
+     * @return true, si le point (x, y) est allumé dans this, false sinon
+     * @pre !this.isEmpty()
+     */
+    @Override
+    public boolean isPixelOn(int x, int y) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+        return false;
+    }
+
+    /**
+     * @param x1 abscisse du premier point
+     * @param y1 ordonnée du premier point
+     * @param x2 abscisse du deuxième point
+     * @param y2 ordonnée du deuxième point
+     * @return true si les deux points (x1, y1) et (x2, y2) sont représentés par la
+     * même feuille de this, false sinon
+     * @pre !this.isEmpty()
+     */
+    @Override
+    public boolean sameLeaf(int x1, int y1, int x2, int y2) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+        return false;
+    }
+
+    /**
+     * @param image2 autre image
+     * @return true si this est incluse dans image2 au sens des pixels allumés false
+     * sinon
+     * @pre !this.isEmpty() && !image2.isEmpty()
+     */
+    @Override
+    public boolean isIncludedIn(AbstractImage image2) {
+        System.out.println();
+        System.out.println("-------------------------------------------------");
+        System.out.println("Fonction a ecrire");
+        System.out.println("-------------------------------------------------");
+        System.out.println();
+        return false;
+    }
 }
